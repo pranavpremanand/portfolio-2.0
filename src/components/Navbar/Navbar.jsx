55e7d53301ac4ae4d9f9143d76017892728e2372
Pranav <mpranavprem@gmail.com>
2024-02-26 14:19:59 +0530

    changed style of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <>
       <div className="h-[10vh] lg:h-[12vh] fixed w-[100%] top-0 z-20 left-0 bg-white flex justify-end items-center shadow-md">
-        <div className="flex w-[100%] justify-between items-center h-[100%] px-7 lg:px-12">
+        <div className="flex w-[100%] justify-between items-center h-[100%] px-9 lg:px-12">
           <Link
             className="font-semibold text-2xl cursor-pointer"
             to="home"
